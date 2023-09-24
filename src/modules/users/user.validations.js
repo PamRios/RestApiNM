3ae81c5addaec8d1cc26705ec8f050ad79cb4b90
PamRios <124759381+PamRios@users.noreply.github.com>
2023-09-24 01:53:22 +0000

    ⭐ Merged

--- src/modules/users/user.validations.js
+++ src/modules/users/user.validations.js
@@ -4,7 +4,9 @@ export const passwordReg = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6, }/;
 export default {
   signUp: {
     body: Joi.object({
-      email: Joi.string().email().required(),
+      email: Joi.string().email().required().messages({
+        'any.required': 'Debe proporcionarse un email valido',
+      }),
       password: Joi.string().regex(passwordReg).required(),
       firstName: Joi.string().required(),
       lastName: Joi.string().required(),
